ccae5106c8042258ee609b796f83eb3dba369148
Lahaduzzaman <criclahad.2@gmail.com>
2021-10-16 23:41:21 +0600

    some change

--- models/room.js
+++ models/room.js
@@ -1,14 +1,36 @@
 const mongoose = require("mongoose");
 
 const roomSchema = mongoose.Schema({
-    name: {
+    name : {
         type: string,
         required: true
     },
-    maxcount: {
+    maxcount : {
         type: Number,
         required: true
-
+    },
+    phonenumber : {
+        type: Number,
+        required: true
+    },
+    rentperday : {
+        type: Number,
+        required: true
+    },
+    imageurls : [],
+    currentbookings : [],
+    type : {
+        type: string,
+        required: true
+    },
+    description : {
+        type: string,
+        required: true
     }
-
+},{
+    timestamps: true,
 })
+
+const roomModel = mongoose.model('rooms', roomSchema);
+
+module.exports = roomModel;
